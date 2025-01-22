7e439330033e7ed950ec75f80bed4c80cb832ff6
Karla Kovacs <karlakovacs@mail.com>
2025-01-22 11:17:47 +0200

    CSS

--- frontend/src/pages/comune/Acasa.jsx
+++ frontend/src/pages/comune/Acasa.jsx
@@ -1,9 +1,11 @@
 import "../../css_files/comune/Acasa.css";
+import welcome from "../../assets/welcome.svg";
 
 const Acasa = () => {
 	return (
 		<div className="Acasa">
 			<h2>Bun venit!</h2>
+            <img src={welcome} alt="Welcome" />
 		</div>
 	);
 };
